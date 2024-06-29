f9f11d928a891d87016260f20a0b3f9e6d6405f6
halcyon-past <titanssuperior@gmail.com>
2024-06-30 03:14:10 +0530

    back to fetch

--- src/app/vetclinics/page.tsx
+++ src/app/vetclinics/page.tsx
@@ -14,13 +14,17 @@ interface BloodBank {
 
 const fetchBloodBank = async (): Promise<BloodBank[]> => {
   try {
-    const response = await axios.get('/api/vets',{
-      headers: {
-        'Cache-Control': 'no-cache',
-      },
+    const response = await fetch('/api/vets', {
+      cache: 'no-store'
     });
-    return response.data.map((item: any) => ({
-      name: ${item.clinicName},
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch blood bank data: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data.map((item: any) => ({
+      name: `${item.clinicName}`,
       location: item.city,
       email: item.email,
       phoneNumber: item.phoneNumber,
